1d94d718a93c4416b68e217112383cc53e32c941
boyang1020 <boyang1020@gmail.com>
2019-01-22 18:38:46 -0800

    count unique values

---
+++ countUniqueValues/countUniqueValues.js
@@ -0,0 +1,14 @@
+// count how many unique values in a given sorted array.
+
+function countUniqueValues(arr){
+    if(arr.length === 0) return 0;
+    var i = 0;
+    for (let j = 0; j < arr.length; j++) {
+        if (arr[j] !== arr[i]) {
+            i++;
+            arr[i] = arr[j];
+        }
+    }
+    return i + 1;
+    }
+    

05a799c498bf54668f5d2c3cfbe3be5cfeaa1b95
boyang1020 <boyang1020@gmail.com>
2019-01-24 15:06:51 -0800

    average pair

---
+++ averagePair/averagePair.js
@@ -0,0 +1,11 @@
+function averagePair(arr, num) {
+    let left = 0;
+    let right = arr.length - 1;
+    while (left < right) {
+        let average = (arr[left] + arr[right])/2;
+        if(average === num) return true;
+        else if (average < num) left++;
+        else right--;
+    }
+    return false;
+}

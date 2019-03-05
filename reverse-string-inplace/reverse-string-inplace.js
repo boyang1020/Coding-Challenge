ac6e120e0c142f1f422b71d2c2f837dcecf46258
boyang1020 <boyang1020@gmail.com>
2019-03-04 19:03:16 -0800

    Write a function that takes an array of characters and reverses the letters in place.

---
+++ reverse-string-inplace/reverse-string-inplace.js
@@ -0,0 +1,21 @@
+// Write a function that takes an array of characters and reverses the letters in place.
+
+function reverse(arrayOfChars) {
+
+    // Reverse the input array of characters in place
+    
+  let leftIndex = 0;
+  let rightIndex = arrayOfChars.length - 1;
+  
+  while(leftIndex < rightIndex) {
+    const temp = arrayOfChars[leftIndex];
+    arrayOfChars[leftIndex] = arrayOfChars[rightIndex];
+    arrayOfChars[rightIndex] = temp;
+    
+    leftIndex++;
+    rightIndex--;
+  }
+  }
+  
+  
+  

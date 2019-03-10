785aa533dfd6d946a30d1d6bab6834ed536888e8
boyang1020 <boyang1020@gmail.com>
2019-03-10 15:42:28 -0700

    reverse word in a string

--- reverse-word-inplace/revers-words-inplace.js
+++ reverse-word-inplace/revers-words-inplace.js
@@ -3,7 +3,7 @@ function reverseWords(message) {
     reverseChar(message, 0, message.length - 1);
     
     let currentWordStartIndex = 0;
-    for(i = 0; i < message.length; i++) {
+    for(let i = 0; i < message.length; i++) {
       if(i === message.length || message[i] === ' ') {
         reverseChar(message, currentWordStartIndex, i - 1);
         currentWordStartIndex = i + 1;

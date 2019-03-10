dcf49fde95c181b59c014ee9ac739615d9220a22
boyang1020 <boyang1020@gmail.com>
2019-03-10 15:42:43 -0700

    reverse word in a string

--- reverse-word-inplace/revers-words-inplace.js
+++ reverse-word-inplace/revers-words-inplace.js
@@ -1,22 +1,26 @@
-function reverseWords(message) {
 
+function reverseWords(message) {
     reverseChar(message, 0, message.length - 1);
-    
     let currentWordStartIndex = 0;
-    for(let i = 0; i < message.length; i++) {
-      if(i === message.length || message[i] === ' ') {
+    for (let i = 0; i <= message.length; i++) {
+      if (i === message.length || message[i] === ' ') {
         reverseChar(message, currentWordStartIndex, i - 1);
         currentWordStartIndex = i + 1;
-        }
       }
     }
-    
-function reverseChar(message, leftIndex, rightIndex) {
+  }
+  
+  function reverseChar(message, leftIndex, rightIndex) {
     while (leftIndex < rightIndex) {
-        const temp = message[leftIndex];
-        message[leftIndex] = message[rightIndex];
-        message[rightIndex] = temp;
-        leftIndex++;
-        rightIndex--;
+      const temp = message[leftIndex];
+      message[leftIndex] = message[rightIndex];
+      message[rightIndex] = temp;
+      leftIndex++;
+      rightIndex--;
     }
-}
+  }
+  
+  
+  
+  
+  
